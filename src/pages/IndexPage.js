234fa73d1a70659be834d6d5d762c6cbe1fac352
alberthuynh91 <alberthuynh91@gmail.com>
2018-02-07 00:25:02 -0800

    Center text and helvetica font

--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -4,7 +4,7 @@ import styles from '../styles/index.scss';
 export default class IndexPage extends Component {
   render() {
     return (
-      <div>
+      <div className={styles.container__index}>
         <h1>Who Sampled Playlist Creator</h1>
         <div>Create playlists of music sampled by your favorite aritsts</div>
         <a href="/login"><button className={styles['button__login-spotify']}><i className="fab fa-spotify"></i> Log in with Spotify</button></a>
