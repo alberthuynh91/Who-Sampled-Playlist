a01a5e1bca43236f22ae283b647faca80b9b7795
alberthuynh91 <alberthuynh91@gmail.com>
2018-02-06 22:12:22 -0800

    Fixed login route

--- src/Routes.js
+++ src/Routes.js
@@ -2,22 +2,17 @@ import React, { Component } from 'react';
 import { Router, Route, hashHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 
-import store from './lib/store';
-const history = syncHistoryWithStore(hashHistory, store);
-import IndexPage from 'pages/IndexPage';
-import TodoApp from 'components/TodoApp';
-import LoginPage from 'pages/LoginPage';
-import HomePage from 'pages/HomePage';
+import IndexPage from './pages/IndexPage';
 import SpotifyAppPage from './pages/SpotifyAppPage';
+import store from './lib/store';
+
+const history = syncHistoryWithStore(hashHistory, store);
 
 export default class Routes extends Component {
   render() {
     return (
       <Router history={history}>
         <Route path="/" component={IndexPage} />
-        <Route path="todo" component={TodoApp} />
-        <Route path="/login" component={LoginPage} />
-        <Route path="/home" component={HomePage} />
         <Route path="/spotify" component={SpotifyAppPage} />
       </Router>
     );
--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router';
 
 export default class IndexPage extends Component {
   render() {
@@ -7,7 +6,7 @@ export default class IndexPage extends Component {
       <div>
         <h1>Who Sampled Playlist Creator</h1>
         <div>Create playlists of music sampled by your favorite aritsts</div>
-        <Link to="/login"><button className="btn" ><i className="fab fa-spotify"></i> Log in with Spotify</button></Link>
+        <a href="/login"><button className="btn" ><i className="fab fa-spotify"></i> Log in with Spotify</button></a>
       </div>
     );
   }
