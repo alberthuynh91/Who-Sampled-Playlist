114155f12894d50b4aea78583e3cd900b6589ed6
alberthuynh91 <alberthuynh91@gmail.com>
2018-02-13 23:32:41 -0800

    Adding background image. Clean up index page

--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -6,7 +6,7 @@ export default class IndexPage extends Component {
     return (
       <div className={styles.container__index}>
         <h1>Who Sampled Playlist Creator</h1>
-        <div>Create playlists of music sampled by your favorite aritsts</div>
+        <h2>Create playlists of music sampled by your favorite aritsts</h2>
         <a href="/login"><button className={styles['button__login-spotify']}><i className="fab fa-spotify"></i> Log in with Spotify</button></a>
       </div>
     );
